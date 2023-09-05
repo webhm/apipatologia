cc8c119380ca33b83ea38dbe045bcd7f87a6296d
Vivian <vivirodbar@gmail.com>
2023-09-05 13:25:21 +0200

    ajustes estado Pedidos

--- app/Http/Controllers/Api/V1/EstadopedidoController.php
+++ app/Http/Controllers/Api/V1/EstadopedidoController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\V1\StoreEstadopedidoRequest;
 use App\Http\Requests\V1\UpdateEstadopedidoRequest;
 use App\Http\Resources\V1\EstadopedidoCollection;
 use App\Http\Resources\V1\EstadopedidoResource;
-use App\Models\Corte;
 use App\Models\Estadopedido;
 use App\Models\Informe;
 use Illuminate\Http\Request;
@@ -17,7 +16,7 @@ class EstadopedidoController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return EstadopedidoCollection
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
@@ -39,7 +38,7 @@ class EstadopedidoController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Models\Estadopedido  $estadopedido
-     * @return EstadopedidoResource
+     * @return \Illuminate\Http\Response
      */
     public function show(Estadopedido $estadopedido)
     {
@@ -68,26 +67,75 @@ class EstadopedidoController extends Controller
     {
         //
     }
+
     /**
      * Search the State of each Pedido from a collection of Pedidos.
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return \Illuminate\Http\Response
      */
     public function getPedidosEstados(Request $request)
     {
+        $data = array(
+            array(
+                "CD_PRE_MED" => "520277",
+                "estado" => "Confirmado",
+                "color" => "#ffaa11"
+            ),
+            array(
+                "CD_PRE_MED" => "519352",
+                "estado" => "Ingresado",
+                "color" => "#ffaa11"
+            ),
+            array(
+                "CD_PRE_MED" => "519066",
+                "estado" => "Parcial",
+                "color" => "#ffaa11"
+            ),
+            array(
+                "CD_PRE_MED" => "518184",
+                "estado" => "Confirmado",
+                "color" => "#ffaa11"
+            ),
+            array(
+                "CD_PRE_MED" => "517288",
+                "estado" => "Ingresado",
+                "color" => "#ffaa11"
+            ),
+            array(
+                "CD_PRE_MED" => "516386",
+                "estado" => "Parcial",
+                "color" => "#ffaa11"
+            )
+        );
+
+
+        // return response()->json($data, 200);
+        // $input = $request->all();
+        //  dd($input); exit();
+
         $input = $request->all();
         $estados = array();
         foreach ($input as $pedido) {
             $numeroPedido = $pedido['CD_PRE_MED'];
             $chequeoEstado = Informe::select('id')
-                ->whereNoPedidoMv($numeroPedido)
+                ->where('NoPedidoMv', $numeroPedido )
                 ->count();
             if ($chequeoEstado == 0) {
-                $estados[] = '{"pedido": $numeroPedido, "estado": "No procesado", "color": "#FF0000")}';
+                $estados[] = array(
+                    "pedido" => $numeroPedido,
+                    "estado" => "Parcial",
+                    "color" => "#ffaa11"
+                ) ;
             } else {
-                $estados[] = '{"pedido": $numeroPedido, "estado": "Procesado", "color": "#00FF00")}';
+                $estados[] =
+                    array(
+                        "pedido" => $numeroPedido,
+                        "estado" => "Procesado",
+                        "color" => "#ffaa11"
+                    ) ;
             }
         }
-        return $estados;
+        return response()->json($estados, 200);
+        //return $estados;
     }
 }
